2b03bb40341a55d34b7292d5c37631c2b28acc0e
Nodd <124269320+Indio01@users.noreply.github.com>
2024-02-27 12:32:49 +0000

    feat: aula-1

--- App.js
+++ App.js
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>ETEC PROFESSOR MILTON GAZZETTI</Text>
+      <Text style={{fontStyle: 'italic'}}>Terceira Série</Text>
+      <Text style={{textDecorationLine: 'underline'}}>mtec Informatica para internet</Text>
       <StatusBar style="auto" />
     </View>
   );
